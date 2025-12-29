115eec7771dbbd5f799b6509d8727eb7ed3d6822
liwen <aa771117@gmail.com>
2025-12-29 21:40:56 +0800

    修正搜尋框

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -55,33 +55,15 @@ function Navbar() {
             </li>
           </ul>
 
-          <form className="d-flex" role="search">
-            <div className="input-group navbar-search position-relative">
-              <span
-                className="input-group-text bg-neutral-500 text-neutral-300 search-span"
-                id="search-addon"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-search"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                </svg>
-              </span>
-              <input
-                type="text"
-                className="form-control bg-neutral-500 search-input text-neutral-200"
-                placeholder="搜尋作品、文章或問題…"
-                aria-label="Search"
-                aria-describedby="search-addon"
-                style={{ width: "324px" }}
-              />
-            </div>
-          </form>
+
+          <div className="input-group custom-search-group">
+            <span className="input-group-text py-12px pe-12px">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+              </svg>
+            </span>
+            <input type="text" className="form-control" placeholder="搜尋作品、文章或問題..." aria-label="Search" />
+          </div>
           <div className="ms-3">
             <AuthButton />
           </div>
