658fd2d5edb8cc295a955d9789cd7ff524c6aa35
liwen <aa771117@gmail.com>
2026-01-23 21:24:44 +0800

    改成router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from 'react-router'
+import routes from './components/routes/index.jsx'
+const router = createHashRouter(routes)
 // 套件導入
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -9,13 +12,14 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import "./main.scss";
 
 //模組導入
-import App from "./App.jsx";
+// import App from "./App.jsx";
 
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HashRouter> {/* 在最外層定義一次即可 */}
-      <App />
-    </HashRouter>
-  </StrictMode>
+  <RouterProvider router={router} />,
+  // <StrictMode>
+  //   <HashRouter> {/* 在最外層定義一次即可 */}
+  //     <App />
+  //   </HashRouter>
+  // </StrictMode>
 );
