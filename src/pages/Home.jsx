7fb938475bd0c70a6c7b8af81c24d8607aa4b22c
Voo Ylin <50961933+ylin320@users.noreply.github.com>
2026-01-27 21:20:28 +0800

    新增:ArticleSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import IndexTop from "../components/IndexTop/IndexTop";
 import PortfolioSection from "../components/PortfolioSection/portfolioSection";
 import QASection from "../components/QASection/QASection";
 import CTASection from "../components/CTASection/CTASection";
+import ArticleSection from "../components/ArticleSection/ArticleSection";
 
 // TODO: 首頁背景圖片未完成
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <div className="container-fulid">
       <IndexTop />
       <PortfolioSection />
+      <ArticleSection />
       <QASection />
       <CTASection />
     </div>
