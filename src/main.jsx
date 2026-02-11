e3883327138ecf64eb12b5bf304208d9f4a35a93
羅文森 <vincentale.21@gmail.com>
2026-02-11 19:55:21 +0800

    u

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import { StrictMode } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { createHashRouter, RouterProvider } from 'react-router'
